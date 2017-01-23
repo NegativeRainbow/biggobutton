a4828aacbc28c6755458576f76539530565f2ed7
NegativeRainbow <danishb@uw.edu>
2017-01-22 18:17:48 -0800

    Completed Assignment

--- app/src/main/java/edu/washington/danishb/biggobutton/MainActivity.java
+++ app/src/main/java/edu/washington/danishb/biggobutton/MainActivity.java
@@ -2,12 +2,26 @@ package edu.washington.danishb.biggobutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
+
+    int pushCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button pushMe = (Button)findViewById(R.id.btnPushMe);
+        pushMe.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                pushCount++;
+                pushMe.setText("You have pushed me " + pushCount + " times!");
+            }
+        });
     }
 }
